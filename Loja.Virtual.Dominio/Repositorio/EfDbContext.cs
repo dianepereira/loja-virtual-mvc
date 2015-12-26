3d4327c10f9eb93bed634e8655cb83463ed126e6
dianepereira <dianepereira.ti@gmail.com>
2015-12-26 15:24:32 -0200

    View listaProdutos.

---
+++ Loja.Virtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using LojaVirtual.Dominio.Entidades;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+
+        }
+
+    }
+}
---
+++ Loja.Virtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Loja.Virtual.Web.MvcApplication" Language="C#" %>
