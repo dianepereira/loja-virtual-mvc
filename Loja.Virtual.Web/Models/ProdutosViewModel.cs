96ee47897891770d0e8ad0d2f6787900f86d8aad
dianepereira <dianepereira.ti@gmail.com>
2015-12-28 23:35:44 -0200

    ViewModel

--- Loja.Virtual.Web/Models/ProdutosViewModel.cs
+++ Loja.Virtual.Web/Models/ProdutosViewModel.cs
@@ -6,7 +6,6 @@ namespace LojaVirtual.Web.Models
     public class ProdutosViewModel
     {
         public IEnumerable<Produto> Produtos { get; set; }
-
         public Paginacao Paginacao { get; set; }
     }
 }
